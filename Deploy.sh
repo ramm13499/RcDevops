da69bb964fd0d2fb3be0747f2202ef38d4bc492f
ramm13499 <lokeshram323@gmail.com>
2025-05-25 10:58:15 +0530

    Deploy.sh

---
+++ Deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Simulated deploy script
+
+echo "Starting deployment..."
+# Put your deployment commands here
+echo "Code pulled from repo"
+echo "Services restarted"
+echo "Deployment complete!"
