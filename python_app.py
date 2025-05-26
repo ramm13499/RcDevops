b9725b26d32ad4f36b810b1efcaaf945638e615c
ramm13499 <lokeshram323@gmail.com>
2025-05-26 17:37:59 +0530

    Update python_app.py

--- python_app.py
+++ python_app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DevOps Pipeline!"
+    return "Hello from Jenkins + Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    print("Flask app ran successfully.")
+    Comment this out if not needed:
+     app.run()
