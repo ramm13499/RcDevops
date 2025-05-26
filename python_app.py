21f1054762a6f4258119a57578f1cf95b9c0e436
ramm13499 <lokeshram323@gmail.com>
2025-05-26 17:41:50 +0530

    Update python_app.py

--- python_app.py
+++ python_app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Jenkins + Flask!"
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    print("Flask app ran successfully.")
-     app.run()
+    app.run()
